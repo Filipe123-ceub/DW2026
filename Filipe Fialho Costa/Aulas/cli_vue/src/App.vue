b7ba6c0431699f8c9a93248a9845be363036bd02
Filipe Fialho Costa <filipe.fialho@dinfor.uniceub>
2026-09-18 19:59:25 -0300

    Atualiza

---
+++ Filipe Fialho Costa/Aulas/cli_vue/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+ <div>
+ <home-component></home-component>
+ </div>
+</template>
+
+<script>
+
+import HomeComponent from './components/HomeComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomeComponent,
+  },
+  mounted() {
+  console.log('Minha aplicação montada com sucesso!');
+  },
+  created() {
+  console.log('Minha aplicação criada com sucesso!');
+  },
+  errorCaptured(error) {
+    alert(`Ocorreu um erro: ${error.message}`);
+    return false;
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
